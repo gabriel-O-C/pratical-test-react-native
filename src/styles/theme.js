1bf326f6d4a738a5f289f75f0a6356da9e50b107
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-15 16:45:08 +0000

    feat: style variables

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    gray1: '#333333',
+    gray2: '##4F4F4F',
+    gray3: '#828282',
+    white: '#ffffff',
+  },
+  spacings: {},
+  fonts: {
+    lato: 'Lato',
+  },
+  sizes: {
+    large: '24px',
+    medium: '14px',
+    medium_large: '18px',
+    small: '12px',
+  },
+};
