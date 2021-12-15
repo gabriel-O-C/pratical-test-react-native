05f7ecaa8a0454252d56645c9ba6d751f7a03b13
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-15 16:39:09 +0000

    feat: fonts added

---
+++ src/styles/global.styles.js
@@ -0,0 +1,5 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Poppins:wght@700&display=swap');
+`;
